c3d0ca0ee82b02d61bde5311449464f488e1f8e5
Shivam Gupta <shivamkumarsggupta@gmail.com>
2021-03-04 23:58:33 +0530

    normal

--- Packages/add_date.py
+++ Packages/add_date.py
@@ -4,4 +4,4 @@ import os
 		
 class AddDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("insert_snippet", { "contents": "%s" %  "/**\n *    author:  Shivam Gupta\n *    created: " + datetime.date.today().strftime("%d.%m.%Y") + " " + datetime.datetime.now().strftime("%H:%M:%S") + "\n**/\n" + open(os.path.join(os.path.dirname(__file__),"cpp_basic.sublime-snippet"), "r").read().split('<![CDATA[')[1].split(']]></content>')[0]} )
+        self.view.run_command("insert_snippet", { "contents": "%s" %  "/**\n *   author:  Shivam Gupta\n *   created: " + datetime.date.today().strftime("%d.%m.%Y") + " " + datetime.datetime.now().strftime("%H:%M:%S") + "\n**/\n" + open(os.path.join(os.path.dirname(__file__),"cpp_basic.sublime-snippet"), "r").read().split('<![CDATA[')[1].split(']]></content>')[0]} )
