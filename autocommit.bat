f9c552fa41f2c961e812d1c128c2122a60e3686f
Shivam Gupta <shivamkumarsggupta@gmail.com>
2021-03-05 00:25:00 +0530

    normal

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,4 @@
-git add -u                               
-git commit -m "normal" 
+cd C:\Users\Shivam Gupta\AppData\Roaming\Sublime Text 3
+git add -u
+git commit -m "normal"
 git push origin master
